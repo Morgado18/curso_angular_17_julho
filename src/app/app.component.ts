43f54b74263355f1bbe7fecb8e77172d22c5949b
Morgado18 <guiandrademelo108@example.com>
2024-07-15 17:06:46 +0100

    aula08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { ListComponent } from './features/list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, HeaderComponent
+    RouterOutlet, HeaderComponent, ListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
