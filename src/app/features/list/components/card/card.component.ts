57feb59625b6f1ff4b40161efeb3fd93a11f994d
Morgado18 <guiandrademelo108@example.com>
2024-07-19 15:07:12 +0100

    aula terminada

--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { Product } from '../../../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [
-    MatCardModule, MatChipsModule
+    MatCardModule, MatChipsModule,
   ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+  product = input.required<Product>() // a entrada deve ser um array conforme definido na interface
 
+  productTitle = computed(()=> this.product().title)
 }
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,12 +4,13 @@ import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
   imports: [
-    MatCardModule, MatChipsModule
+    MatCardModule, MatChipsModule, CardComponent
   ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
