2a4058f421c1db281db9dd619af636247ab0a4ad
Morgado18 <guiandrademelo108@example.com>
2024-07-20 10:34:53 +0100

    Aula 14 completada

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule, MatInputModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true, validators: Validators.required
+    })
+  });
+
+  onSubmit() { /* pode ser qualquer nome, será chamada quando houver uma sumbimssão de form */
+    this.form.controls.title.value;
+  }
 
 }
